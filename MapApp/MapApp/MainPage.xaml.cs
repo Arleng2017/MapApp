e012dd38c2f2deed8d67824db58257fcb1df368d
arleng2017 <arleng2017@gmail.com>
2020-06-13 18:59:37 +0700

    Initial commit

---
+++ MapApp/MapApp/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Plugin.Geolocator;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace MapApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private async void GetGPSBtn_Clicked(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
+                var position = await locator.GetPositionAsync();
+
+                LongiText.Text = position.Latitude.ToString();
+                LagiText.Text = position.Longitude.ToString();
+                longPosition = position.Longitude;
+                latiPosition = position.Latitude;
+
+                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude), Distance.FromKilometers(100)));
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Notification", "Unable to get GPS Location " + ex, "Ok");
+            }
+
+
+        }
+
+       
+    }
+}
